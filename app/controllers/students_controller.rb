d582340fd0aa0254deb03071a35b66305e2f99df
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-08-28 16:41:35 -0400

    commit tree 6fa02c5ce4b1b9ffd5465a115f8b525e772797d3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      query = "%#{params[:name]}%"  # Create a SQL LIKE query pattern
+      students = Student.where("first_name LIKE ? OR last_name LIKE ?", query, query)
+    else
+      students = Student.all
+    end
+
+    render json: students
+  end
+
+  # def index
+  #   students = Student.all
+  #   render json: students
+  # end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
